d349fa52a60f7ba46ab42d80dbdc818fed7c35c2
chiefJurist <anthonynnanna004@gmail.com>
2023-08-24 09:57:38 +0100

    tailwind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      //adding additional styles
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      }
+      //end of additional style
+    },
   },
   plugins: [],
 }
