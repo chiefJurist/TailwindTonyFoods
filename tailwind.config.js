039c15880f99b366db680e894047ec14d468dff5
chiefJurist <anthonynnanna004@gmail.com>
2023-08-24 10:17:23 +0100

    custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,11 @@ module.exports = {
           100: '#E2E2D5',
           200: '#888883'
         }
+      },
+      fontFamily: {
+        //you can name your font any name of your choice. In our case we are going with "body"
+        //you can add other fonts as well to make up your font faimily. In our case we are using only Nunito
+        body: ['Nunito']
       }
       //end of additional style
     },
